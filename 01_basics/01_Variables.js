da59e9436da3f92bd3f315e71b9032814f7306b8
CodeUp-india <akashrajput281003@gmail.com>
2024-10-04 07:49:06 +0000

    DataType

---
+++ 01_basics/01_Variables.js
@@ -0,0 +1,11 @@
+const accountId = 12345
+let accountEmail = "akash@gmail.com"
+var accountPassword = "134"
+accountCity = "luthvania"
+
+
+accountEmail = "choco@google.com"
+accountPassword = "345"
+accountCity = "Delhi"
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
