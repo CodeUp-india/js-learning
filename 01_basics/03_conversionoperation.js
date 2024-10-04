9cb0a1af1a67a887be741dc87418358d41640ce6
CodeUp-india <akashrajput281003@gmail.com>
2024-10-04 08:22:18 +0000

    converstion of Datatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,15 @@
+let score = null
+console.log(typeof score);
+
+let convertString = Number(score)
+
+// 45 => Number
+// "45abc" => NaN
+// "Akash" => String
+// 1 => true, 0 => false
+// null = 0
+
+console.log(score)
+console.log(typeof convertString);
+console.log(convertString)
+
