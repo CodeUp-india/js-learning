588a0435bc1714f484b76de50d309ca96880bdfc
CodeUp-india <akashrajput281003@gmail.com>
2024-10-06 07:12:42 +0000

    summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,26 @@
+// # Primitive Datatype
+
+// 7  types : String , Number, Boolean, null, undefined, Symbol, Big Int
+
+ 
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId);
+
+
+
+
+// Refrence (Non primitive)
+
+// Array, Objects, Functoins
+
+const heros = ["Ironman", "chocomon", "Perman"]
+let myObj = {
+    name: "akash",
+    age: 20,
+
+}
+ const myFunciton =  function(){
+    console.log("Hello choco");
+    
+ }
