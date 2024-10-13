089dfea1ae3922c2bcc1fe2111557697e1f8b835
CodeUp-india <akashrajput281003@gmail.com>
2024-10-13 13:50:38 +0000

    arrays again

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,3 +1,13 @@
 //arrray
 const myArry = [0, 1, 2, 3, 4, 5]
+//console.log(myArry);
+
+const heros = ["chocoman", "bokoman", "dogman"]
+//console.log(heros);
+const merg = myArry.concat(heros)
+//console.log(merg);
+let think = myArry.splice(0, 3)
 console.log(myArry);
+console.log(think);
+
+
