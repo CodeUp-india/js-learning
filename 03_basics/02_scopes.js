819f364c737d1aea4fa47662db6ca2f06c415f06
CodeUp-india <akashrajput281003@gmail.com>
2024-10-18 11:56:36 +0000

    scope 2

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,35 @@
+if (true) {
+    let a = 1
+    const b = 2
+    var c = 3
+}
+
+
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+
+function one(){
+    const username = "akash"
+    function two(){
+        const website  = "github"
+        console.log(username);        
+    }
+    //console.log(website);
+    two()
+    
+}
+//one()
+
+function addone(num) {
+    return num +1
+}
+console.log(addone(3));
+
+const addTwo = (num)=>{
+    return num + 2
+} 
+console.log(addTwo(5));
