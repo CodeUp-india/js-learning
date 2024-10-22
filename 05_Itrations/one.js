632c1de11fef3e69b6f4b7c52a8cea5d6f409b63
CodeUp-india <akashrajput281003@gmail.com>
2024-10-22 06:19:13 +0000

    itration

---
+++ 05_Itrations/one.js
@@ -0,0 +1,32 @@
+// for loops
+
+for (let index = 0; index <=10; index++) {
+    const element = index;
+    if (element == 5) {
+        //console.log("5 is the best number");
+        
+    }
+    //console.log(element);
+    
+    
+}
+
+for (let i = 1; i <= 10; i++) {
+   // console.log(`Table of : ${i}`);
+    
+    for (let j = 1; j <= 10; j++) {
+        //console.log(i + '*' + j  + '=' + i*j );     
+    }
+    
+}
+
+
+let myArry = ['choco', 'wico', 'abbys']
+for (let i = 0; i < myArry.length; i) {
+    const element = myArry[i];
+    console.log(element);
+    
+}
+
+//break and continue
+
