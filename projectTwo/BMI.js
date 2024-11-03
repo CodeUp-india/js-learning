b86d48d405c8dd18166de1bd1d3f6f9b91001e6f
CodeUp-india <akashrajput281003@gmail.com>
2024-11-03 15:14:39 +0000

    project Two

---
+++ projectTwo/BMI.js
@@ -0,0 +1,27 @@
+const form = document.querySelector('form')
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    const height = parseInt(document.querySelector('#height').value)
+    const weight = parseInt(document.querySelector('#weight').value)
+    const result = document.querySelector('#results')
+    
+    if (height === '' || height < 0 || isNaN(height)) {
+        result.innerHTML = `Please Enter Valid Height ${height} `    
+    }
+    else if  (weight === '' || weight < 0 || isNaN(weight)) {  
+    }
+    else{
+        const bmi = (weight / ((height*height) / 10000)).toFixed(2)
+        result.innerHTML = `<span>${bmi}<span>`
+        if (bmi <= 18.5) {
+            result.innerHTML = `Your BMI is ${bmi} Put on some weight you skinny AF`
+        } else if (bmi >= 18.5 && bmi <= 24.5) {
+            result.innerHTML = `Your BMI is ${bmi}, You are fine `
+        }else{
+            result.innerHTML = `Damm!!!! ${bmi} is your BMI. Go to GYM you Chaeso looking fat motherfucker`
+        }
+    }
+
+
+})
