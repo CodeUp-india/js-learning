8b78b92842fa85d192ec6db0a9008536db7bee8f
CodeUp-india <akashrajput281003@gmail.com>
2024-11-08 10:55:45 +0000

    practice

--- projectFive/change.js
+++ projectFive/change.js
@@ -1,24 +1,24 @@
 const rect = document.querySelector('.center')
 rect.addEventListener('mousemove', (e)=>{
     let rectLocation = rect.getBoundingClientRect()
-    
     let insideRect = e.clientX - rectLocation.left
-    if (insideRect<rectLocation.width/2){
-        let redcolor = gsap.utils.mapRange(0, rectLocation.width/2, 0, 255,  insideRect)
+    if (insideRect< rectLocation.width/2) {
+        let redcolor = gsap.utils.mapRange(0, rectLocation.width/2, 0, 255, insideRect)
         gsap.to(rect, {
             backgroundColor: `rgb(255, ${redcolor}, ${redcolor})`,
-            ease: Power4,
+            ease: Power4
         })
-    }else{
-        let bluecolor = gsap.utils.mapRange(rectLocation.width/2, rectLocation.width, 255, 0,  insideRect)
+        
+    } else {
+        let bluecolor = gsap.utils.mapRange(rectLocation.width/2, rectLocation.width, 0, 255, insideRect)
         gsap.to(rect, {
             backgroundColor: `rgb(${bluecolor}, ${bluecolor}, 255)`,
-            ease: Power4,
-        })
+            ease: Power4
+        })   
     }
-})
-rect.addEventListener('mouseleave', ()=>{
-    gsap.to(rect, {
-        backgroundColor: "white",
+    rect.addEventListener('mouseleave', ()=>{
+        gsap.to(rect, 
+            {backgroundColor: "white"}
+        )
     })
 })
